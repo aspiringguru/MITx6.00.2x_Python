4889fbf6b1661a2339414fa460f9d17ece176058
aspiringguru <bmatthewtaylor@gmail.com>
2016-04-04 10:19:33 +1000

    Robot class added

---
+++ Robot.py
@@ -0,0 +1,80 @@
+#https://github.com/aspiringguru/MITx6.00.2x_Python
+#passes tests. Need to write a test script.
+
+class Robot(object):
+    """
+    Represents a robot cleaning a particular room.
+
+    At all times the robot has a particular position and direction in the room.
+    The robot also has a fixed speed.
+
+    Subclasses of Robot should provide movement strategies by implementing
+    updatePositionAndClean(), which simulates a single time-step.
+    """
+    #When a Robot is initialized, it should clean the first tile it is initialized on.
+    def __init__(self, room, speed):
+        """
+        Initializes a Robot with the given speed in the specified room. The
+        robot initially has a random direction and a random position in the
+        room. The robot cleans the tile it is on.
+
+        room:  a RectangularRoom object.
+        speed: a float (speed > 0)
+        """
+        self.position = room.getRandomPosition()
+        #set random value for direction
+        self.direction = random.random()*360
+        self.room = room
+        self.speed = speed
+
+        #set position to clean
+
+        #raise NotImplementedError
+
+    def getRobotPosition(self):
+        """
+        Return the position of the robot.
+
+        returns: a Position object giving the robot's position.
+        """
+        return self.position
+        #raise NotImplementedError
+
+    def getRobotDirection(self):
+        """
+        Return the direction of the robot.
+
+        returns: an integer d giving the direction of the robot as an angle in
+        degrees, 0 <= d < 360.
+        """
+        return self.direction
+        #raise NotImplementedError
+
+    def setRobotPosition(self, position):
+        """
+        Set the position of the robot to POSITION.
+
+        position: a Position object.
+        """
+        self.position = position
+        #raise NotImplementedError
+
+    def setRobotDirection(self, direction):
+        """
+        Set the direction of the robot to DIRECTION.
+
+        direction: integer representing an angle in degrees
+        """
+        self.direction = direction
+        #raise NotImplementedError
+
+    def updatePositionAndClean(self):
+        """
+        Simulate the raise passage of a single time-step.
+
+        Move the robot to a new position and mark the tile it is on as having
+        been cleaned.
+        """
+        #NB: problem description specifically nominates _NOT_ to implement this.
+        raise NotImplementedError # don't change this!
+
